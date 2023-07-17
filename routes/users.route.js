55f00d39e055c0824d6f04ae00620b6afda4b430
seongwong woo <dnqkr18@gmail.com>
2023-07-17 18:25:41 +0900

    login,signup

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,4 +1,14 @@
+//users.route.js
+
 const express = require('express');
 const router = express.Router();
+const UserController = require('../controllers/user.controller');
+
+//회원가입
+router.post('/signup', UserController.register);
+//로그인
+router.post('/login', UserController.login);
+//로그아웃
+router.post('/logout', UserController.logout);
 
 module.exports = router;
