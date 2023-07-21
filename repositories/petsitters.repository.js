23abc3e04b837e5b5a0f5b74420ca7b76688f5f1
dayounglee <verdantjuly@kakao.com>
2023-07-21 09:24:34 +0900

    fix : petsitters invisible after deleting reivew solved
    solved by checking raw query
    WHERE r.deletedAt IS NULL > removed
    
    Frontend Tested

--- repositories/petsitters.repository.js
+++ repositories/petsitters.repository.js
@@ -7,7 +7,7 @@ class PetsitterRepository {
       `SELECT p.petsitterId, p.name, p.imgurl, p.description, p.signInCareer, IFNULL(AVG(r.star),0) AS starAvg
           FROM Petsitters AS p
           LEFT JOIN Reviews as r on p.petsitterId = r.petsitterId 
-              WHERE p.deletedAt IS NULL AND r.deletedAt IS NULL
+              WHERE p.deletedAt IS NULL
               GROUP BY p.petsitterId 
               ORDER BY starAvg DESC`,
       { type: QueryTypes.SELECT },
